e55d001f63ce3953f3dc15f63fcf5c7392b1cc4b
Prince kumar <122357150+itzprincephutela@users.noreply.github.com>
2024-07-13 02:50:43 +0530

    project ready to deploy

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ def update(sno):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create database tables
-    app.run(debug=True, port=8000)
+    app.run(debug=True, port=8000,host="0.0.0.0")
